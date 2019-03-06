ad900c43c1ca799e91d55be576b7eb9f5ee675e0
Charlie Roffe <charlie@roffe.net>
2019-03-05 22:31:38 -0500

    Updated name, photo, and sidebar links/images

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  title: 'Blog by John Doe',
+  url: 'https://croffe.com',
+  title: 'Blog by Charlie Roffe',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-135691667-1',
   menu: [
     {
       label: 'Articles',
@@ -23,14 +23,14 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Charlie Roffe',
     photo: '/photo.jpg',
     bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
-      email: '#',
+      email: 'charlie@roffe.net',
       telegram: '#',
-      twitter: '#',
-      github: '#',
+      twitter: '@charlieroffe',
+      github: 'croffe',
       rss: '#',
       vkontakte: '#'
     }
